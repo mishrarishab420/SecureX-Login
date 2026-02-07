f008867cdda67eef696359821b64801781aa3948
mishrarishab420 <mishrarishab420@gmail.com>
2026-02-07 21:47:47 +0530

    Fix PostgreSQL config for Render

--- app.py
+++ app.py
@@ -2,20 +2,29 @@ import os
 from flask import Flask, render_template
 from database.models import db
 from auth.register import register_user
+from auth.login import login_user
+from flask import session, redirect
+from database.models import db, User, LoginLog
 
 app = Flask(__name__)
 
 # Secret key (later we will move this to env)
 app.config["SECRET_KEY"] = "securex-secret-key"
 
-# Cloud-ready DB config
-db_url = os.environ.get("DATABASE_URL", "sqlite:///securex.db")
+# Cloud-ready DB config (Render PostgreSQL + Local SQLite fallback)
 
-# Fix for Render + Postgres
-if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
+db_url = os.environ.get("DATABASE_URL")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+if db_url:
+    # Fix for Render postgres URL
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+else:
+    # Local fallback
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/securex.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
@@ -28,6 +37,27 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return register_user()
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    return login_user()
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user_id" not in session:
+        return redirect("/")
+
+    return render_template(
+        "dashboard.html",
+        username=session["username"]
+    )
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":
